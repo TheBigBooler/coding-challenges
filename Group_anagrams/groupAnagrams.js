e62b1d2bf121e8efa1f7761e7c59ae2366337e33
James <>
2023-11-02 22:12:45 -0500

    group anagrams, concept understood, but having trouble implementing the code

---
+++ Group_anagrams/groupAnagrams.js
@@ -0,0 +1,16 @@
+var groupAnagrams = function (strs) {
+//break up array to sort each string, then join it back together as a sorted word
+//create a hash map to save values of sorted words, then add the word to matching value
+    if(!strs) {
+        return []
+    } else if(strs.length=1) {
+        return strs
+    } else {
+        console.log(strs)
+    }
+   
+};
+
+const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+
+groupAnagrams(strs)
