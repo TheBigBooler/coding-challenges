e26beaff5076b79035e8b10bd1abaa81a3a0fa24
James <>
2023-12-04 02:05:17 -0600

    starting apple and orange

---
+++ AppleAndOrange/appleAndOrange.js
@@ -0,0 +1,7 @@
+//s,t are range of house
+//a = apple tree, b = orange tree
+//apples/oranges are array with distances from tree
+
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+  // Write your code here
+}
