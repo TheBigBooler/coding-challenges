985faab33383b7a76c8949ce8a0044066754dd48
James <>
2023-12-06 00:23:19 -0600

    12/30 test cases passed

--- NumberLineJumps/numberLineJumps.js
+++ NumberLineJumps/numberLineJumps.js
@@ -6,9 +6,10 @@ function kangaroo(x1, v1, x2, v2) {
     //if first kangaroo ahead and moving faster, return NO
   } else if (x1 >= x2 && v1 > v2) {
     return "NO"
-    //
+    //if theyre moving at same speed, will never meet, return NO
   } else if (v1 === v2) {
     return "NO"
+    //if one kangaroo is moving faster and don't intersect after x jumps, return NO
   } else {
     return "YES"
   }
