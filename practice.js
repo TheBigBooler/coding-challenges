398d20de91b874f5980b9d2d2424c85d136ff362
James <>
2023-12-09 18:43:37 -0600

    quick little calculator i made to add steam game values for lawsuit

--- practice.js
+++ practice.js
@@ -33,3 +33,20 @@
 
 // miniMaxSum([1, 3, 5, 7, 9])
 // miniMaxSum challenge done!
+
+const total = document.getElementById("total");
+const gamesToAdd = document.getElementById("inputBar");
+const totalButton = document.getElementById("totalButton");
+
+function addGames() {
+    const numbers = gamesToAdd.value
+    const numArray = numbers.split(",") 
+    const addNums = numArray.reduce((a, b) => {const num1 = parseInt(a)
+    const num2 = parseInt(b)
+    return num1 + num2 })
+    console.log(addNums)
+    total.innerText=addNums
+}
+
+totalButton.addEventListener("click", addGames)
+
