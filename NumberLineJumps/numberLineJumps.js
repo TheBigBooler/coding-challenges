3a2bd648d5e56c10baf7a8fa59eb9a451e9b191a
James <>
2024-01-16 00:40:21 -0600

    used discussion board to help with solving this one

--- NumberLineJumps/numberLineJumps.js
+++ NumberLineJumps/numberLineJumps.js
@@ -1,16 +1,15 @@
 function kangaroo(x1, v1, x2, v2) {
-  // Write your code here
-  //if second kangaroo is ahead and moving faster, return NO
-  if (x2 >= x1 && v2 > v1) {
-    return "NO"
-    //if first kangaroo ahead and moving faster, return NO
-  } else if (x1 >= x2 && v1 > v2) {
-    return "NO"
-    //if theyre moving at same speed, will never meet, return NO
-  } else if (v1 === v2) {
-    return "NO"
-    //if one kangaroo is moving faster and don't intersect after x jumps, return NO
+  let result = "";
+  //if kangaroo is ahead and moving faster, return no
+  if (x2 > x1 && v2 >= v1) {
+    result = "NO";
+  //if the difference in their starting position divides evenly by the difference in the speed, they will meet  
+  } else if ((x2 - x1) % (v1 - v2) == 0) {
+    result = "YES";
   } else {
-    return "YES"
+    result = "NO";
   }
+  return result;
 }
+
+kangaroo(0, 3, 4, 2);
